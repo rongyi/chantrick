f50262e9cb226550200230fbfad3ab5bf51dae6f
rongyi <sqrongyi@163.com>
2017-11-19 09:58:35 +0800

    Add tee command

--- chan.go
+++ chan.go
@@ -154,3 +154,29 @@ func OrDone(done, c <-chan interface{}) <-chan interface{} {
 
 	return vs
 }
+
+// Tee just like tee command for channel
+func Tee(done, in <-chan interface{}) (_, _ <-chan interface{}) {
+	out1 := make(chan interface{})
+	out2 := make(chan interface{})
+
+	go func() {
+		defer close(out1)
+		defer close(out2)
+		for val := range OrDone(done, in) {
+			var out1, out2 = out1, out2
+			for i := 0; i < 2; i++ {
+				select {
+				case <-done:
+				case out1 <- val:
+					// to make out1 block
+					out1 = nil
+				case out2 <- val:
+					// to make out2 block
+					out2 = nil
+				}
+			}
+		}
+	}()
+	return out1, out2
+}
--- chan_test.go
+++ chan_test.go
@@ -66,3 +66,13 @@ func TestOrDone(t *testing.T) {
 		fmt.Println(v)
 	}
 }
+
+func TestTee(t *testing.T) {
+	done := make(chan interface{})
+	defer close(done)
+
+	out1, out2 := Tee(done, Take(done, Repeat(done, 1, 2), 4))
+	for val1 := range out1 {
+		fmt.Printf("out1: %v, out2 %v\n", val1, <-out2)
+	}
+}
